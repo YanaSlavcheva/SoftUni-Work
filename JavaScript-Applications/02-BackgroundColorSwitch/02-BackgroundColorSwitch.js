e6a1e867d35124dbbcc38dae09992a4ce6cc8c11
Yana Slavcheva <yana.slavcheva@gmail.com>
2014-11-27 00:08:53 +0200

    Moved

--- JavaScript-Applications/02-BackgroundColorSwitch/02-BackgroundColorSwitch.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    console.log( "ready!" );
-    $("#form").submit(function (e) {
-        var classInput = $('#class').val();
-        var color = $('#color').val();
-        $("#" + classInput).css("background-color",color);
-        e.preventDefault();
-    })
-})

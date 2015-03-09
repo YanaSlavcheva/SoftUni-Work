42525b9d2fd5c6a8d39e1c285b9f860d75c60e61
Yana Slavcheva <yana.slavcheva@gmail.com>
2015-03-09 23:23:47 +0200

    added Database-Applications Course Problems - Entity Framework

---
+++ Database-Applications/01-EntityFramework/SoftUniDB/EntryPoint.cs
@@ -0,0 +1,33 @@
+﻿namespace SoftUniDB
+{
+    using System;
+    using System.Linq;
+
+    public class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            var db = new SoftUniEntities();
+
+            var today = new DateTime();
+            today = DateTime.Now;
+
+            var testEmployee = new Employee
+            {
+                FirstName = "Test",
+                MiddleName = "Test",
+                LastName = "Test",
+                JobTitle = "Test",
+                DepartmentID = 1,
+                HireDate = today,
+                Salary = 1000
+            };
+
+            ManageEmployees.InsertEmployee(testEmployee);
+            ManageEmployees.DeleteEmployee("Test", "Test", "Test");
+            ManageEmployees.ModifyEmployee(298, "FirstName", "Pesho");
+            ManageEmployees.PrintEmployee(298);
+            
+        }
+    }
+}
---
+++ Database-Applications/01-EntityFramework/SoftUniDB/Interfaces/IEmployeeDAO.cs
@@ -0,0 +1,20 @@
+﻿namespace SoftUniDB
+{
+    using System.Security.Cryptography.X509Certificates;
+
+    public interface IEmployeeDao
+    {
+        void InsertEmployee(Employee employee);
+
+        void ModifyEmployee(int employeeId, string propertyName, object newValue);
+
+        void DeleteEmployee(string firstName, string middleName, string lastName);
+
+        void PrintEmployee(int employeeId);
+
+        // delete by ID
+        // delete by FirstName
+        // delete by LastName
+        // delete by JobTitle
+    }
+}

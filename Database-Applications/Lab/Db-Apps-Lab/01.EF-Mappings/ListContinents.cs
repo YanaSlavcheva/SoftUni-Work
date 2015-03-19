91cd7a95242a28506efdeefa103ad95034a2e12b
Yana Slavcheva <yana.slavcheva@gmail.com>
2015-03-19 18:41:11 +0200

    problem 01 ready

---
+++ Database-Applications/Lab/Db-Apps-Lab/01.EF-Mappings/ListContinents.cs
@@ -0,0 +1,20 @@
+﻿namespace EF_Mappings
+{
+    using System;
+
+    public class ListContinents
+    {
+        public static void Main(string[] args)
+        {
+            var context = new GeographyEntities();
+            var continents = context.Continents;
+
+            foreach (var continent in continents)
+            {
+                Console.WriteLine(continent.ContinentName);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}

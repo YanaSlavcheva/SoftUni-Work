ee7360d20d752ef391cf3303a9ef56f94d558c19
Yana Slavcheva <yana.slavcheva@gmail.com>
2015-03-24 21:12:32 +0200

    last updates

--- Database-Applications/01-EntityFramework/SoftUniDB/EntryPoint.cs
+++ Database-Applications/01-EntityFramework/SoftUniDB/EntryPoint.cs
@@ -9,23 +9,32 @@
         {
             var db = new SoftUniEntities();
 
-            var today = DateTime.Now;
+            ////Problem 02
+            //var today = DateTime.Now;
+            //var testEmployee = new Employee
+            //{
+            //    FirstName = "Test",
+            //    MiddleName = "Test",
+            //    LastName = "Test",
+            //    JobTitle = "Test",
+            //    DepartmentID = 1,
+            //    HireDate = today,
+            //    Salary = 1000
+            //};
 
-            var testEmployee = new Employee
-            {
-                FirstName = "Test",
-                MiddleName = "Test",
-                LastName = "Test",
-                JobTitle = "Test",
-                DepartmentID = 1,
-                HireDate = today,
-                Salary = 1000
-            };
+            //ManageEmployeesDao.InsertEmployee(db, testEmployee);
+            //ManageEmployeesDao.DeleteEmployee(db, "Test", "Test", "Test");
+            //ManageEmployeesDao.ModifyEmployee(db, 298, "FirstName", "Pesho");
+            //ManageEmployeesDao.PrintEmployee(db, 298);   
+ 
+            ////Problem 03
+            //ManageEmployeesDao.FindEmployeesStartDateInYear(db, new DateTime(2012, 01, 01));
 
-            ManageEmployeesDao.InsertEmployee(testEmployee);
-            ManageEmployeesDao.DeleteEmployee("Test", "Test", "Test");
-            ManageEmployeesDao.ModifyEmployee(298, "FirstName", "Pesho");
-            ManageEmployeesDao.PrintEmployee(298);    
+            //Problem 04
+            //ManageEmployeesDao.FindEmployeesStartDateInYearNativeSql(db, new DateTime(2012, 01, 01));
+
+            //Problem 05
+            ManageEmployeesDao.FindEmployeesByDepartmentAndManager(db, "Production", "Jo", "Brown");
         }
     }
 }
--- Database-Applications/01-EntityFramework/SoftUniDB/ManageEmployeesDAO.cs
+++ Database-Applications/01-EntityFramework/SoftUniDB/ManageEmployeesDAO.cs
@@ -5,16 +5,14 @@
 
     public class ManageEmployeesDao
     {
-        public static void InsertEmployee(Employee employee)
+        public static void InsertEmployee(SoftUniEntities db, Employee employee)
         {
-            var db = new SoftUniEntities();
             db.Employees.Add(employee);
             db.SaveChanges();
         }
 
-        public static void ModifyEmployee(int employeeId, string propertyName, object newValue)
+        public static void ModifyEmployee(SoftUniEntities db, int employeeId, string propertyName, object newValue)
         {
-            var db = new SoftUniEntities();
             var employeeToModify = db.Employees.Find(employeeId);
             if (employeeToModify != null)
             {
@@ -27,22 +25,76 @@
             }
         }
 
-        public static void DeleteEmployee(string firstName, string middleName, string lastName)
+        public static void DeleteEmployee(SoftUniEntities db, string firstName, string middleName, string lastName)
         {
-            var db = new SoftUniEntities();
             Employee employeeToDelete =
                 db.Employees.First(e => e.FirstName == firstName && e.MiddleName == middleName && e.LastName == lastName);
             db.Employees.Remove(employeeToDelete);
             db.SaveChanges();
         }
 
-        public static void PrintEmployee(int employeeId)
+        public static void PrintEmployee(SoftUniEntities db, int employeeId)
         {
-            var db = new SoftUniEntities();
             var employeeToPrint = db.Employees.Find(employeeId);
             foreach (var prop in employeeToPrint.GetType().GetProperties())
             {
                 Console.WriteLine("{0} = {1}", prop.Name, prop.GetValue(employeeToPrint, null));
+            }
+        }
+
+        public static void FindEmployeesStartDateInYear(SoftUniEntities db, DateTime date)
+        {
+            var year = date.Year;
+
+            var employeesWProjectsWStartDateInYear =
+                db.Employees.Where(e => e.Projects.Any(p => p.StartDate.Year == year));
+
+            foreach (var employee in employeesWProjectsWStartDateInYear)
+            {
+                Console.WriteLine("Employee Name: {0} {1}", employee.FirstName, employee.LastName);
+                Console.WriteLine("Projects Names: ");
+                foreach (var project in employee.Projects)
+                {
+                    Console.WriteLine("{0}; Start Date: {1}", project.Name, project.StartDate);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public static void FindEmployeesStartDateInYearNativeSql(SoftUniEntities db, DateTime date)
+        {
+            var year = date.Year;
+            const string SqlQuery = "SELECT e.FirstName, e.LastName, p.Name, p.StartDate FROM Employees e JOIN EmployeesProjects ep ON e.EmployeeID = ep.EmployeeID JOIN Projects p ON ep.ProjectID = p.ProjectID WHERE p.StartDate BETWEEN ('2002-01-01') AND ('2002-12-31')";
+
+            var employeesWProjectsWStartDateInYear = db.Database.SqlQuery<string>(SqlQuery).ToList();
+
+            foreach (var employee in employeesWProjectsWStartDateInYear)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+
+        public static void FindEmployeesByDepartmentAndManager(
+            SoftUniEntities db,
+            string departmentName,
+            string managerFirstName,
+            string managerLastName)
+        {
+            var manager =
+                db.Employees.FirstOrDefault(m => m.FirstName == managerFirstName && m.LastName == managerLastName);
+            var employees =
+                db.Employees.Where(e => e.Department.Name == departmentName && e.ManagerID == manager.EmployeeID);
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine("Employee Name: {0} {1}", employee.FirstName, employee.LastName);
+                Console.WriteLine("Department Name: {0}", employee.Department.Name);
+                if (manager != null)
+                {
+                    Console.WriteLine("Manager Name: {0} {1}", manager.FirstName, manager.LastName);
+                }
+                Console.WriteLine();
             }
         }
     }

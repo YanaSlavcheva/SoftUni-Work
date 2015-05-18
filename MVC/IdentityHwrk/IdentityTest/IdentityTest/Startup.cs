5b75d6a61e7c40f2574513cacdb93a460ee7c77d
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-05-18 21:20:35 +0300

    initial project with fixed connection string

---
+++ MVC/IdentityHwrk/IdentityTest/IdentityTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityTest.Startup))]
+namespace IdentityTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

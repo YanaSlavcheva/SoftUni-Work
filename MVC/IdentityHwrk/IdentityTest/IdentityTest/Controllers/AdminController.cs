5b30e13cf6bfebd795da06be0eee7652ce6f10ba
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-05-18 23:33:40 +0300

    Added AdminController.cs and Admin page only for admins

---
+++ MVC/IdentityHwrk/IdentityTest/IdentityTest/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿namespace IdentityTest.Controllers
+{
+    using System.Web.Mvc;
+
+    using Microsoft.AspNet.Identity;
+
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (this.User.IsInRole("Administrator"))
+            {
+                return this.View(model: this.User.Identity.GetUserName());
+            }
+
+            return this.RedirectToAction("Index", "Home");
+        }
+    }
+}

6c43a92774af439b8e41636f7435d9545e36d2ba
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-06-13 23:12:18 +0300

    bookmarks detail view started

---
+++ MVC/ExamPrep-Bookmarks/Bookmarks/Bookmarks.Web/ViewModels/BookmarkDetailsViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Bookmarks.Web.ViewModels
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using Bookmarks.Common.Mappings;
+    using Bookmarks.Models;
+
+    public class BookmarkDetailsViewModel : IMapFrom<Bookmark>
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        //public virtual IEnumerable<CommentViewModel> Comments { get; set; }
+
+        public int Votes { get; set; }
+    }
+}
